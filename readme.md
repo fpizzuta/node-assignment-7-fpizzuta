39dcaf39eba3776b6c40b5afbebf8114d3768902
Frank Pizzuta <frank@pizzuta.com>
2019-05-05 16:39:18 -0400

    readme

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # Assignment #7 - Final Project
 
-The description of this assignment can be found in Canvas at [Assignment #7/Final](https://canvas.harvard.edu/courses/54354/assignments/249116) (Spring 2019)
+Create is done with New Notification link.
+Read happens when the index page loads.
+Update is done with Edit link.
+Delete is done with Delete link.
 
-You should build your application in this repo cloned for you in Github Classroom. You'll submit your project and github URLs in Canvas.
--- server/app.js
+++ server/app.js
@@ -34,14 +34,14 @@ app.use(bodyparser.json());
 app.set('views', './views');    // tells express where to find the views
 app.set('view engine', 'pug');  // tells express to use pug as the template engine
 
-app.use('/delete', posts);
-app.use('/newpost', posts);
-app.use('/edit', posts);
-app.use('/posts', posts);
-app.use('/create', posts);
-app.use('/users', users);
-app.use('/user', users);
-app.use('/login', login);
+// app.use('/delete', posts);
+// app.use('/newpost', posts);
+// app.use('/edit', posts);
+// app.use('/posts', posts);
+// app.use('/create', posts);
+// app.use('/users', users);
+// app.use('/user', users);
+// app.use('/login', login);
 // app.use('/', login);
 app.use('/api/posts', apiposts);
 // app.use('/', express.static('./public'));
